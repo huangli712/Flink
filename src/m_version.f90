147e6cbd12a086a283fe6854326d53b7ab71d7c0
Li Huang <lihuang.dmft@gmail.com>
2019-04-10 18:19:17 +0800

    update src/m_version.f90

--- src/m_version.f90
+++ src/m_version.f90
@@ -1,11 +1,11 @@
 !!!-----------------------------------------------------------------------
-!!! project : CSML (Common Service Modules Library)
+!!! project : flink @ sakura
 !!! program : version
 !!! source  : m_version.f90
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 01/26/2017 by li huang (created)
-!!!           07/10/2017 by li huang (last modified)
+!!!           04/10/2019 by li huang (last modified)
 !!! purpose : the purpose of this module is to define version strings.
 !!! status  : unstable
 !!! comment :
@@ -36,7 +36,7 @@
 !!
 !! version string, version number + date info. + status info.
 !!
-     character(len=20), public, parameter :: V_FULL = 'v0.7.0 @ 2017.01.31D'
+     character(len=20), public, parameter :: V_FULL = 'v0.7.0 @ 2019.04.10D'
 
 !!
 !! @var V_CURR
@@ -50,7 +50,7 @@
 !!
 !! version string, only date info.
 !!
-     character(len=11), public, parameter :: V_DATE = '2017.01.31'
+     character(len=11), public, parameter :: V_DATE = '2019.04.10'
 
 !!
 !! @var V_STAT
@@ -64,7 +64,7 @@
 !!
 !! version string, author info.
 !!
-     character(len=46), public, parameter :: V_AUTH = 'by li huang (at IOP/CAS & SPCLab/CAEP & UNIFR)'
+     character(len=46), public, parameter :: V_AUTH = 'by li huang (China Academy of Engineering Physics)'
 
 !!
 !! @var V_MAIL
