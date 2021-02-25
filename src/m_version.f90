194d98c45f09524a39cbd736c712fd665557b029
Li Huang <lihuang.dmft@gmail.com>
2021-02-25 23:55:19 +0800

    fix version strings

--- src/m_version.f90
+++ src/m_version.f90
@@ -5,7 +5,7 @@
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 01/26/2017 by li huang (created)
-!!!           04/15/2019 by li huang (last modified)
+!!!           02/25/2021 by li huang (last modified)
 !!! purpose : the purpose of this module is to define version strings.
 !!! status  : unstable
 !!! comment :
@@ -23,6 +23,7 @@
 !!    _IQ => iQIST
 !!    _FL => FLINK
 !!    _DF => DFAPP
+!!    _ZD => ZEN/DMFT
 !!
 !! Usage
 !! =====
@@ -43,8 +44,9 @@
 !! version string, version number + date info. + status info.
 !!
      character(len=20), public, parameter :: V_FULL_IQ = 'v0.8.0 @ 2019.04.10D'
-     character(len=20), public, parameter :: V_FULL_FL = 'v0.1.1 @ 2019.04.12D'
+     character(len=20), public, parameter :: V_FULL_FL = 'v0.1.2 @ 2021.02.25D'
      character(len=20), public, parameter :: V_FULL_DF = 'v0.0.1 @ 2019.04.12D'
+     character(len=20), public, parameter :: V_FULL_ZD = 'v0.0.1 @ 2021.02.25D'
 
 !!
 !! @var V_CURR
@@ -52,8 +54,9 @@
 !! version string, only version number
 !!
      character(len=06), public, parameter :: V_CURR_IQ = 'v0.8.0'
-     character(len=06), public, parameter :: V_CURR_FL = 'v0.1.1'
+     character(len=06), public, parameter :: V_CURR_FL = 'v0.1.2'
      character(len=06), public, parameter :: V_CURR_DF = 'v0.0.1'
+     character(len=06), public, parameter :: V_CURR_ZD = 'v0.0.1'
 
 !!
 !! @var V_DATE
@@ -61,8 +64,9 @@
 !! version string, only date info.
 !!
      character(len=11), public, parameter :: V_DATE_IQ = '2019.04.10'
-     character(len=11), public, parameter :: V_DATE_FL = '2019.04.12'
+     character(len=11), public, parameter :: V_DATE_FL = '2021.02.25'
      character(len=11), public, parameter :: V_DATE_DF = '2019.04.12'
+     character(len=11), public, parameter :: V_DATE_ZD = '2021.02.25'
 
 !!
 !! @var V_STAT
@@ -72,6 +76,7 @@
      character(len=01), public, parameter :: V_STAT_IQ = 'D'
      character(len=01), public, parameter :: V_STAT_FL = 'D'
      character(len=01), public, parameter :: V_STAT_DF = 'D'
+     character(len=01), public, parameter :: V_STAT_ZD = 'D'
 
 !!
 !! @var V_AUTH
@@ -81,6 +86,7 @@
      character(len=11), public, parameter :: V_AUTH_IQ = 'by li huang'
      character(len=11), public, parameter :: V_AUTH_FL = 'by li huang'
      character(len=11), public, parameter :: V_AUTH_DF = 'by li huang'
+     character(len=11), public, parameter :: V_AUTH_ZD = 'by li huang'
 
 !!
 !! @var V_INST
@@ -90,6 +96,7 @@
      character(len=36), public, parameter :: V_INST_IQ = 'China Academy of Engineering Physics'
      character(len=36), public, parameter :: V_INST_FL = 'China Academy of Engineering Physics'
      character(len=36), public, parameter :: V_INST_DF = 'China Academy of Engineering Physics'
+     character(len=36), public, parameter :: V_INST_ZD = 'China Academy of Engineering Physics'
 
 !!
 !! @var V_MAIL
@@ -99,6 +106,7 @@
      character(len=22), public, parameter :: V_MAIL_IQ = 'lihuang.dmft@gmail.com'
      character(len=22), public, parameter :: V_MAIL_FL = 'lihuang.dmft@gmail.com'
      character(len=22), public, parameter :: V_MAIL_DF = 'lihuang.dmft@gmail.com'
+     character(len=22), public, parameter :: V_MAIL_ZD = 'lihuang.dmft@gmail.com'
 
 !!
 !! @var V_GPL3
@@ -108,5 +116,6 @@
      character(len=36), public, parameter :: V_GPL3_IQ = 'GNU General Public License version 3'
      character(len=36), public, parameter :: V_GPL3_FL = 'GNU General Public License version 3'
      character(len=36), public, parameter :: V_GPL3_DF = 'GNU General Public License version 3'
+     character(len=36), public, parameter :: V_GPL3_ZD = 'GNU General Public License version 3'
 
   end module version
