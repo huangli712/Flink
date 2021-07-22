39f48a0c3ff80ec075096eb13d3172a828401bda
Li Huang <lihuang.dmft@gmail.com>
2021-07-22 23:44:17 +0800

    improve code style for m_linkedlist.f90

--- src/m_linkedlist.f90
+++ src/m_linkedlist.f90
@@ -5,98 +5,13 @@
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 07/10/2014 by li huang (created)
-!!!           04/10/2019 by li huang (last modified)
+!!!           07/22/2021 by li huang (last modified)
 !!! purpose : this purpose of this module is to implement a typical and
 !!!           useful data structure --- linked list. it is a generic
 !!!           linked list, capable of storing arbitrary data.
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
-
-!!
-!!
-!! Introduction
-!! ============
-!!
-!! This implementation of generic linked list in fortran 90 was taken from
-!! Jason R. Blevins's code
-!!     journal: ACM Fortran Forum 28(3), 2-7, 2009;
-!!     website: http://jblevins.org/research/generic-list.
-!! Of course, we have adapted the original code to fulfill our requirement.
-!! This linked list is capable of storing data of an any type by using the
-!! generic programming techniques. To access the data stored in the nodes,
-!! we have to use the intrinsic transfer() subroutine.
-!!
-!! Usage
-!! =====
-!!
-!! 1. include linked list support
-!! ------------------------------
-!!
-!! use linkedlist
-!!
-!! 2. define user own data type
-!! ----------------------------
-!!
-!! type data_t
-!!     ! place your definition here
-!! end type data_t
-!!
-!! 3. define pointer to data
-!! -------------------------
-!!
-!! type (data_t), pointer  :: data_ptr => null()
-!!
-!! 4. define pointer to list
-!! -------------------------
-!!
-!! type (list_t), pointer  :: list_ptr => null()
-!!
-!! 5. prepare data
-!! ---------------
-!!
-!! allocate(data_ptr)
-!! data_ptr%something = something
-!!
-!! 6. create a linked list
-!! -----------------------
-!!
-!! call list_init(list_ptr, transfer(data_ptr, list_d))
-!!
-!! here list_d is a public variable in linkedlist module.
-!!
-!! 7. insert new node
-!! ------------------
-!!
-!! call list_insert(list_ptr, transfer(data_ptr, list_d))
-!!
-!! list_insert() will always insert the new node after the given node,
-!! i.e., list_ptr. If you want to update the data for the given node,
-!! please use the following code:
-!!
-!! call list_put(list_ptr, transfer(data_ptr, list_d))
-!!
-!! 8. visit next node
-!! ------------------
-!!
-!! curr => list_next(curr)
-!!
-!! here curr is a list_t type pointer.
-!!
-!! 9. retrieve data stored in the node
-!! -----------------------------------
-!!
-!! data_ptr  = transfer(list_get(curr), data_ptr)
-!! something = data_ptr%something
-!!
-!! here curr is a list_t type pointer, it points to the current node.
-!!
-!! 10. free memory for this linked list
-!! ------------------------------------
-!!
-!! call list_free(list_ptr)
-!!
-!!
 
   module linkedlist
      implicit none
@@ -105,11 +20,13 @@
 !!>>> declare global data types                                        <<<
 !!========================================================================
 
-! a public variable used as a mold for transfer() subroutine
+!! module variables
+     ! a public variable used as a mold for transfer() subroutine.
      integer, allocatable :: list_d(:)
 
-! node for the linked list, it contains a pointer pointing to next node,
-! and the integer pointer array is used to store data
+!! module structs
+     ! node for the linked list. it contains a pointer pointing to next
+     ! node, and the integer pointer array is used to store data.
      type list_t
          private
          integer, pointer :: data(:) => null()
@@ -138,29 +55,33 @@
 !! @sub list_init
 !!
 !! initialize a head node [self] for a list and optionally store the
-!! provided data
+!! provided data.
 !!
   subroutine list_init(self, data)
      implicit none
 
-! external arguments
-! pointer to new linked list
+!! external arguments
+     ! pointer to new linked list
      type (list_t), pointer :: self
 
-! the data for the first element
+     ! the data for the first element
      integer, intent(in), optional :: data(:)
 
-! allocate memory for linked list
+!! [body
+
+     ! allocate memory for linked list
      allocate(self)
      nullify(self%next)
 
-! check whether we should make an empty node
+     ! check whether we should make an empty node
      if ( present(data) ) then
          allocate( self%data( size(data) ) )
          self%data = data
      else
          nullify(self%data)
      endif ! back if ( present(data) ) block
+
+!! body]
 
      return
   end subroutine list_init
