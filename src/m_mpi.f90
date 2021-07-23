d76ead3e12381e4ea1e57b80c7c0f89b0aa92daf
Li Huang <lihuang.dmft@gmail.com>
2021-07-24 00:00:23 +0800

    improve code style for m_mpi.f90

--- src/m_mpi.f90
+++ src/m_mpi.f90
@@ -4735,42 +4735,42 @@
 !!
 !! @sub mp_reduce_rdp4
 !!
-!! reduce real(dp) matrix from all processes
+!! reduce real(dp) matrix from all processes.
 !!
      subroutine mp_reduce_rdp4(source, data, root, mop, gid)
          implicit none
 
-! external arguments
+!! external arguments
          real(dp), intent(in) :: source(:,:,:,:)
          real(dp), intent(inout) :: data(:,:,:,:)
          integer, intent(in) :: root
          integer, optional, intent(in) :: mop
          integer, optional, intent(in) :: gid
 
-! set current operator
+         ! set current operator
          if ( present(mop) .eqv. .true. ) then
              opera = mop
          else
              opera = MPI_SUM
          endif ! back if ( present(mop) .eqv. .true. ) block
 
-! set current communicator
+         ! set current communicator
          if ( present(gid) .eqv. .true. ) then
              group = gid
          else
              group = MPI_COMM_WORLD
          endif ! back if ( present(gid) .eqv. .true. ) block
 
-! barrier until all processes reach here
+         ! barrier until all processes reach here
          call mp_barrier(group)
 
-! setup element count
+         ! setup element count
          isize = size(source)
 
-! invoke related mpi subroutines
+         ! invoke related mpi subroutines
          call MPI_REDUCE(source, data, isize, m_rdp, opera, root, group, ierror)
 
-! handler for return code
+         ! handler for return code
          call mp_error('mp_reduce_rdp4', ierror)
 
          return
@@ -4779,42 +4779,42 @@
 !!
 !! @sub mp_reduce_rdp5
 !!
-!! reduce real(dp) matrix from all processes
+!! reduce real(dp) matrix from all processes.
 !!
      subroutine mp_reduce_rdp5(source, data, root, mop, gid)
          implicit none
 
-! external arguments
+!! external arguments
          real(dp), intent(in) :: source(:,:,:,:,:)
          real(dp), intent(inout) :: data(:,:,:,:,:)
          integer, intent(in) :: root
          integer, optional, intent(in) :: mop
          integer, optional, intent(in) :: gid
 
-! set current operator
+         ! set current operator
          if ( present(mop) .eqv. .true. ) then
              opera = mop
          else
              opera = MPI_SUM
          endif ! back if ( present(mop) .eqv. .true. ) block
 
-! set current communicator
+         ! set current communicator
          if ( present(gid) .eqv. .true. ) then
              group = gid
          else
              group = MPI_COMM_WORLD
          endif ! back if ( present(gid) .eqv. .true. ) block
 
-! barrier until all processes reach here
+         ! barrier until all processes reach here
          call mp_barrier(group)
 
-! setup element count
+         ! setup element count
          isize = size(source)
 
-! invoke related mpi subroutines
+         ! invoke related mpi subroutines
          call MPI_REDUCE(source, data, isize, m_rdp, opera, root, group, ierror)
 
-! handler for return code
+         ! handler for return code
          call mp_error('mp_reduce_rdp5', ierror)
 
          return
