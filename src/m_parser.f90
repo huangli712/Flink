f5a0329b950ba4aee2f609423e7079f30e8fe2fa
Li Huang <lihuang.dmft@gmail.com>
2021-07-23 10:09:28 +0800

    improve code style for m_parser.f90

--- src/m_parser.f90
+++ src/m_parser.f90
@@ -36,17 +36,18 @@
 !!========================================================================
 
 !! module structs
-! data structure for the items in config/input files: key-value pair
+     ! data structure for the items in config/input files: key-value pair
      type data_t
          logical           :: is_valid
          character(len=32) :: str_key
          character(len=32) :: str_value
      end type data_t
 
-! pointer for the data_t structure
+!! module variables
+     ! pointer for the data_t structure
      type(data_t), pointer :: data_ptr => null()
 
-! pointer for the list_t structure
+     ! pointer for the list_t structure
      type(list_t), pointer :: list_ptr => null()
 
 !!========================================================================
@@ -68,21 +69,25 @@
 !!
 !! @sub p_create
 !!
-!! init the linked list structure and insert a fake element
+!! init the linked list structure and insert a fake element.
 !!
   subroutine p_create()
      implicit none
 
-! allocate memory for data_ptr
+!! [body
+
+     ! allocate memory for data_ptr
      allocate(data_ptr)
 
-! setup a fake element
+     ! setup a fake element
      data_ptr%is_valid = .false.
      data_ptr%str_key = "i_am_key"
      data_ptr%str_value = "i_am_value"
 
-! init the linked list structure and then insert the fake element
+     ! init the linked list structure and then insert the fake element
      call list_init(list_ptr, transfer(data_ptr, list_d))
+
+!! body]
 
      return
   end subroutine p_create
@@ -90,12 +95,16 @@
 !!
 !! @sub p_destroy
 !!
-!! free the linked list data structure
+!! free the linked list data structure.
 !!
   subroutine p_destroy()
      implicit none
 
+!! [body
+
      call list_free(list_ptr)
+
+!! body]
 
      return
   end subroutine p_destroy
@@ -104,119 +113,135 @@
 !! @sub p_parse
 !!
 !! parse the input/config file, and store the key-value pairs to the
-!! linked list data structure
+!! linked list data structure.
 !!
   subroutine p_parse(in_file)
      use, intrinsic :: iso_fortran_env, only : iostat_end
 
      implicit none
 
-! external arguments
-! filename for the input/config file
+!! external arguments
+     ! filename for the input/config file
      character(len=*), intent(in) :: in_file
 
-! local variables
-! loop index
+!! local variables
+     ! loop index
      integer :: p
      integer :: q
 
-! file status flag
+     ! file status flag
      integer :: istat
 
-! original string for line reading
+     ! original string for line reading
      character(len=100) :: string
 
-! string representation for key of key-value pair
+     ! string representation for key of key-value pair
      character(len=100) :: str_key
 
-! string representation for value of key-value pair
+     ! string representation for value of key-value pair
      character(len=100) :: str_value
 
-! open input/config file, here we do not judge whether the file exists
+!! [body
+
+     ! open input/config file, here we do not judge whether the file exists
      open(mytmp, file = trim(in_file), form = 'formatted', status = 'unknown')
 
      FILE_PARSING: do ! start reading the file
 
-! read one line from the input file until we meet the end-of-file (EOF)
-! flag, the line content is stored in string
+         ! try to read one line from the input file until we meet the
+         ! end-of-file (EOF) flag, the line content is stored in string.
          read(mytmp,'(a100)',iostat = istat) string
          if ( istat == iostat_end ) then
              EXIT FILE_PARSING
          else ! it is not the end
-! get rid of the empty, tab, and null in the string
+             ! get rid of the empty, tab, and null in the string
              call s_str_compress(string)
 
-! is it an empty string? if yes, then we turn to the next line
+             ! is it an empty string?
+             ! if yes, then we turn to the next line.
              if ( len_trim(string) == 0   ) then
                  CYCLE
              endif ! back if ( len_trim(string) == 0   ) block
 
-! is it a comment line starting with '#'? if yes, then we skip it
+             ! is it a comment line starting with '#'?
+             ! if yes, then we skip it
              if ( index(string, '#') == 1 ) then
                  CYCLE
              endif ! back if ( index(string, '#') == 1 ) block
 
-! is it a comment line starting with '!'? if yes, then we skip it
+             ! is it a comment line starting with '!'?
+             ! if yes, then we skip it
              if ( index(string, '!') == 1 ) then
                  CYCLE
              endif ! back if ( index(string, '!') == 1 ) block
 
-! get rid of the comment part starting with '#'
+             ! get rid of the comment part starting with '#'
              p = index(string, '#')
              if ( p > 0 ) then
                  string = string(1:p-1)
              endif ! back if ( p > 0 ) block
 
-! get rid of the comment part starting with '!'
+             ! get rid of the comment part starting with '!'
              p = index(string, '!')
              if ( p > 0 ) then
                  string = string(1:p-1)
              endif ! back if ( p > 0 ) block
 
-! extract the key and value pair from the input string
-! note that the user can use both ':' and '=' symbols to separate the key
-! and value pair. however, the ':' and '=' symbols can not appear at the
-! same time for the same string.
+             !
+             ! remarks:
+             !
+             ! note that the user can use both ':' and '=' symbols to
+             ! separate the key and value pair. however, the ':' and
+             ! '=' symbols can not appear at the same time for the same
+             ! string.
+             !
+
+             ! extract the key and value pair from the input string
              p = index(string, ':')
              q = index(string, '=')
-! case 1: we do not find any ":" or "=" character
+             !
+             ! case 1: we do not find any ":" or "=" character
              if ( p == 0 .and. q == 0 ) then
                  write(mystd,'(a)') 'parser: p_parse, wrong file format for '//trim(in_file)
                  STOP
              endif ! back if ( p == 0 .and. q == 0 ) block
-! case 2: we find both ":" and "=" characters
+             !
+             ! case 2: we find both ":" and "=" characters
              if ( p >  0 .and. q >  0 ) then
                  write(mystd,'(a)') 'parser: p_parse, wrong file format for '//trim(in_file)
                  STOP
              endif ! back if ( p >  0 .and. q >  0 ) block
-! case 3: we find only ":" character
+             !
+             ! case 3: we find only ":" character
              if ( p > 0 ) then
                  str_key = string(1:p-1)
                  str_value = string(p+1:len(string))
              endif ! back if ( p > 0 ) block
-! case 4: we find only "=" character
+             !
+             ! case 4: we find only "=" character
              if ( q > 0 ) then
                  str_key = string(1:q-1)
                  str_value = string(q+1:len(string))
              endif ! back if ( q > 0 ) block
 
-! convert str_key and str_value to lowercase
+             ! convert str_key and str_value to lowercase
              call s_str_lowcase(str_key)
              call s_str_compress(str_key)
              call s_str_lowcase(str_value)
              call s_str_compress(str_value)
 
-! check the length of str_key and str_value
+             ! check the length of str_key and str_value
              if ( len_trim(str_key) == 0   ) then
                  write(mystd,'(a)') 'parser: p_parse, wrong file format for '//trim(in_file)
                  STOP
              endif ! back if ( len_trim(str_key) == 0   ) block
+             !
              if ( len_trim(str_value) == 0 ) then
                  write(mystd,'(a)') 'parser: p_parse, wrong file format for '//trim(in_file)
                  STOP
              endif ! back if ( len_trim(str_value) == 0 ) block
 
-! store the key-value pair in the linked list structure
+             ! store the key-value pair in the linked list structure
              allocate(data_ptr)
              data_ptr%is_valid = .true.
              data_ptr%str_key = trim(str_key)
@@ -226,8 +251,10 @@
 
      enddo FILE_PARSING ! over do loop
 
-! close the input/config file
+     ! close the input/config file
      close(mytmp)
+
+!! body]
 
      return
   end subroutine p_parse
