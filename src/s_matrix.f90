4d384276d8ec734ac699ca71e6630ed6b22f6aa7
Li Huang <lihuang.dmft@gmail.com>
2021-07-23 10:23:32 +0800

    improve code style for s_matrix.f90

--- src/s_matrix.f90
+++ src/s_matrix.f90
@@ -42,113 +42,12 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 07/10/2014 by li huang (created)
-!!!           04/10/2019 by li huang (last modified)
+!!!           07/23/2021 by li huang (last modified)
 !!! purpose : these subroutines are used to encapsulate some important and
 !!!           frequently used linear algebra operations.
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
-
-!!
-!!
-!! Introduction
-!! ============
-!!
-!! 1. build constants (0) matrix
-!! -----------------------------
-!!
-!! subroutine s_zeros_i(...)
-!! subroutine s_zeros_d(...)
-!! subroutine s_zeros_z(...)
-!!
-!! 2. build constants (1) matrix
-!! -----------------------------
-!!
-!! subroutine s_ones_i(...)
-!! subroutine s_ones_d(...)
-!! subroutine s_ones_z(...)
-!!
-!! 3. build constants (any values) matrix
-!! --------------------------------------
-!!
-!! subroutine s_any_i(...)
-!! subroutine s_any_d(...)
-!! subroutine s_any_z(...)
-!!
-!! 4. build diagonal matrix
-!! ------------------------
-!!
-!! subroutine s_eye_i(...)
-!! subroutine s_eye_d(...)
-!! subroutine s_eye_z(...)
-!!
-!! 5. build identity matrix
-!! ------------------------
-!!
-!! subroutine s_identity_i(...)
-!! subroutine s_identity_d(...)
-!! subroutine s_identity_z(...)
-!!
-!! 6. build diagonal matrix from vector
-!! ------------------------------------
-!!
-!! subroutine s_diag_i(...)
-!! subroutine s_diag_d(...)
-!! subroutine s_diag_z(...)
-!!
-!! 7. calculate trace for matrix
-!! -----------------------------
-!!
-!! subroutine s_trace_d(...)
-!! subroutine s_trace_z(...)
-!!
-!! 8. calculate determinant for matrix
-!! -----------------------------------
-!!
-!! subroutine s_det_d(...)
-!! subroutine s_det_z(...)
-!!
-!! 9. calculate matrix inversion
-!! -----------------------------
-!!
-!! subroutine s_inv_d(...)
-!! subroutine s_inv_z(...)
-!!
-!! 10. general eigensystem problem
-!! -------------------------------
-!!
-!! subroutine s_eig_dg(...)
-!! subroutine s_eig_zg(...)
-!! subroutine s_eigvals_dg(...)
-!! subroutine s_eigvals_zg(...)
-!!
-!! 11. symmetric eigensystem problem
-!! ---------------------------------
-!!
-!! subroutine s_eig_sy(...)
-!! subroutine s_eig_he(...)
-!! subroutine s_eigvals_sy(...)
-!! subroutine s_eigvals_he(...)
-!!
-!! 12. linear equation solver
-!! --------------------------
-!!
-!! subroutine s_solve_dg(...)
-!! subroutine s_solve_zg(...)
-!! subroutine s_solve_sy(...)
-!! subroutine s_solve_he(...)
-!!
-!! 13. general singular value decomposition
-!! ----------------------------------------
-!!
-!! subroutine s_svd_dg(...)
-!! subroutine s_svd_zg(...)
-!!
-!! note: _i means integer version, _d real(dp) version, and _z complex(dp)
-!! version. _dg means real(dp) general version, _zg complex(dp) general
-!! version, _sy real(dp) symmetric version, _he complex(dp) Hermitian version.
-!!
-!!
 
 !!========================================================================
 !!>>> matrix construction: build zeros/ones/any matrix                 <<<
@@ -157,19 +56,23 @@
 !!
 !! @sub s_zeros_i
 !!
-!! build an integer matrix with all elements are zero
+!! build an integer matrix with all elements are zero.
 !!
   subroutine s_zeros_i(n, A)
      implicit none
 
-! external arguments
-! size of matrix
+!! external arguments
+     ! size of matrix
      integer, intent(in)  :: n
 
-! input/output matrix
+     ! input/output matrix
      integer, intent(out) :: A(n,n)
 
+!! [body
+
      A = 0
+
+!! body]
 
      return
   end subroutine s_zeros_i
@@ -177,7 +80,7 @@
 !!
 !! @sub s_zeros_d
 !!
-!! build a real(dp) matrix with all elements are zero
+!! build a real(dp) matrix with all elements are zero.
 !!
   subroutine s_zeros_d(n, A)
      use constants, only : dp
@@ -185,14 +88,18 @@
 
      implicit none
 
-! external arguments
-! size of matrix
+!! external arguments
+     ! size of matrix
      integer, intent(in)   :: n
 
-! input/output matrix
+     ! input/output matrix
      real(dp), intent(out) :: A(n,n)
 
+!! [body
+
      A = zero
+
+!! body]
 
      return
   end subroutine s_zeros_d
@@ -200,7 +107,7 @@
 !!
 !! @sub s_zeros_z
 !!
-!! build a complex(dp) matrix with all elements are zero
+!! build a complex(dp) matrix with all elements are zero.
 !!
   subroutine s_zeros_z(n, A)
      use constants, only : dp
@@ -208,14 +115,18 @@
 
      implicit none
 
-! external arguments
-! size of matrix
+!! external arguments
+     ! size of matrix
      integer, intent(in)      :: n
 
-! input/output matrix
+     ! input/output matrix
      complex(dp), intent(out) :: A(n,n)
 
+!! [body
+
      A = czero
+
+!! body]
 
      return
   end subroutine s_zeros_z
