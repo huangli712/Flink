51344c616e7ea4a7dbb1014c37ba17accddfbcd8
Li Huang <lihuang.dmft@gmail.com>
2021-07-24 23:47:20 +0800

    improve code style for m_mpi.f90

--- src/m_mpi.f90
+++ src/m_mpi.f90
@@ -1379,6 +1379,83 @@
      end subroutine mp_bcast_log5
 
 !!
+!! @sub mp_bcast_chr0
+!!
+!! broadcasts character from the process with rank "root".
+!!
+     subroutine mp_bcast_chr0(data, root, gid)
+         implicit none
+
+!! external arguments
+         character(len = *), intent(in) :: data
+         !
+         integer, intent(in) :: root
+         integer, optional, intent(in) :: gid
+
+!! [body
+
+         ! set current communicator
+         if ( present(gid) .eqv. .true. ) then
+             group = gid
+         else
+             group = MPI_COMM_WORLD
+         endif ! back if ( present(gid) .eqv. .true. ) block
+
+         ! barrier until all processes reach here
+         call mp_barrier(group)
+
+         ! invoke realted MPI subroutines
+         call MPI_BCAST(data, len(data), m_chr, root, group, ierror)
+
+         ! handler for return code
+         call mp_error('mp_bcast_chr0', ierror)
+
+!! body]
+
+         return
+     end subroutine mp_bcast_chr0
+
+!!
+!! @sub mp_bcast_chr1
+!!
+!! broadcasts character(:) from the process with rank "root".
+!!
+     subroutine mp_bcast_chr1(data, root, gid)
+         implicit none
+
+!! external arguments
+         character(len = *), intent(in) :: data(:)
+         !
+         integer, intent(in) :: root
+         integer, optional, intent(in) :: gid
+
+!! [body
+
+         ! set current communicator
+         if ( present(gid) .eqv. .true. ) then
+             group = gid
+         else
+             group = MPI_COMM_WORLD
+         endif ! back if ( present(gid) .eqv. .true. ) block
+
+         ! barrier until all processes reach here
+         call mp_barrier(group)
+
+         ! setup element count
+         isize = size(data) * len(data(1))
+
+         ! invoke realted MPI subroutines
+         call MPI_BCAST(data, isize, m_chr, root, group, ierror)
+
+         ! handler for return code
+         call mp_error('mp_bcast_chr1', ierror)
+
+!! body]
+
+         return
+     end subroutine mp_bcast_chr1
+
+!!
 !! @sub mp_bcast_int0
 !!
 !! broadcasts int from the process with rank "root".
@@ -1614,83 +1691,6 @@
 
          return
      end subroutine mp_bcast_int5
-
-!!
-!! @sub mp_bcast_chr0
-!!
-!! broadcasts character from the process with rank "root".
-!!
-     subroutine mp_bcast_chr0(data, root, gid)
-         implicit none
-
-!! external arguments
-         character(len = *), intent(in) :: data
-         !
-         integer, intent(in) :: root
-         integer, optional, intent(in) :: gid
-
-!! [body
-
-         ! set current communicator
-         if ( present(gid) .eqv. .true. ) then
-             group = gid
-         else
-             group = MPI_COMM_WORLD
-         endif ! back if ( present(gid) .eqv. .true. ) block
-
-         ! barrier until all processes reach here
-         call mp_barrier(group)
-
-         ! invoke realted MPI subroutines
-         call MPI_BCAST(data, len(data), m_chr, root, group, ierror)
-
-         ! handler for return code
-         call mp_error('mp_bcast_chr0', ierror)
-
-!! body]
-
-         return
-     end subroutine mp_bcast_chr0
-
-!!
-!! @sub mp_bcast_chr1
-!!
-!! broadcasts character(:) from the process with rank "root".
-!!
-     subroutine mp_bcast_chr1(data, root, gid)
-         implicit none
-
-!! external arguments
-         character(len = *), intent(in) :: data(:)
-         !
-         integer, intent(in) :: root
-         integer, optional, intent(in) :: gid
-
-!! [body
-
-         ! set current communicator
-         if ( present(gid) .eqv. .true. ) then
-             group = gid
-         else
-             group = MPI_COMM_WORLD
-         endif ! back if ( present(gid) .eqv. .true. ) block
-
-         ! barrier until all processes reach here
-         call mp_barrier(group)
-
-         ! setup element count
-         isize = size(data) * len(data(1))
-
-         ! invoke realted MPI subroutines
-         call MPI_BCAST(data, isize, m_chr, root, group, ierror)
-
-         ! handler for return code
-         call mp_error('mp_bcast_chr1', ierror)
-
-!! body]
-
-         return
-     end subroutine mp_bcast_chr1
 
 !!
 !! @sub mp_bcast_rdp0
