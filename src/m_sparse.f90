77090f080a61977171be313d3aed7f0ab369e493
Li Huang <lihuang.dmft@gmail.com>
2021-07-26 00:10:50 +0800

    improve code style for m_sparse.f90

--- src/m_sparse.f90
+++ src/m_sparse.f90
@@ -993,47 +993,49 @@
 !!
 !! @sub sp_matmul_amudia
 !!
-!! performs the matrix by matrix product B = A * Diag
+!! performs the matrix by matrix product B = A * Diag.
 !!
   subroutine sp_matmul_amudia(nrow, nmax, a, ja, ia, diag, b, jb, ib)
      implicit none
 
-! external arguments
-! the row dimension of dense matrix
+!! external arguments
+     ! the row dimension of dense matrix
      integer, intent(in)   :: nrow
 
-! maximum number of nonzero elements allowed
-! this should be set to be the lengths of the arrays b and jb
+     ! maximum number of nonzero elements allowed.
+     ! this should be set to be the lengths of the arrays b and jb.
      integer, intent(in)   :: nmax
 
-! a, ja, ia, matrix A in compressed sparse row format
+     ! a, ja, ia, matrix A in compressed sparse row format
      integer, intent(in)   :: ia(nrow+1)
      integer, intent(in)   :: ja(nmax)
      real(dp), intent(in)  :: a(nmax)
 
-! diagonal matrix stored as a vector diag
+     ! diagonal matrix stored as a vector diag
      real(dp), intent(in)  :: diag(nrow)
 
-! b, jb, ib, resulting matrix B in compressed sparse row format
+     ! b, jb, ib, resulting matrix B in compressed sparse row format
      integer, intent(out)  :: ib(nrow+1)
      integer, intent(out)  :: jb(nmax)
      real(dp), intent(out) :: b(nmax)
 
-! local variables
-! loop index
+!! local variables
+     ! loop index
      integer :: i
      integer :: k
 
-! loop index
+     ! loop index
      integer :: k1
      integer :: k2
 
-! init B sparse matrix
+!! [body
+
+     ! init B sparse matrix
      b = 0.0_dp
      ib = 0
      jb = 0
 
-! scale each element
+     ! scale each element
      do i=1,nrow
          k1 = ia(i)
          k2 = ia(i+1) - 1
@@ -1049,6 +1051,8 @@
      do k=ia(1),ia(nrow+1)-1
          jb(k) = ja(k)
      enddo ! over k={ia(1),ia(nrow+1)-1} loop
+
+!! body]
 
      return
   end subroutine sp_matmul_amudia
