c9870734dc166de67f05e0dae24b6d8b287b0119
Li Huang <lihuang.dmft@gmail.com>
2025-01-14 11:45:08 +0800

    fix csr_plus

--- src/m_sparse.f90
+++ src/m_sparse.f90
@@ -3234,7 +3234,19 @@
 !! [body
 
      ! check dimensions
-     if ( nrows <= 0 .or. ncols <= 0 .or. nnz <= 0 ) then
+     if ( csra%nrows /= csrb%nrows .or. &
+          csra%nrows /= csrc%nrows .or. &
+          csrb%nrows /= csrc%nrows .or. &
+          csra%ncols /= csrb%ncols .or. &
+          csra%ncols /= csrc%ncols .or. &
+          csrb%ncols /= csrc%ncols ) then
+         write(mystd,'(a)') 'sparse: wrong dimensions for sparse matrix'
+         STOP
+     endif ! back if block
+     !
+     if ( csra%nnz /= csrb%nnz .or. &
+          csra%nnz /= csrc%nnz .or. &
+          csrb%nnz /= csrc%nnz ) then
          write(mystd,'(a)') 'sparse: wrong dimensions for sparse matrix'
          STOP
      endif ! back if block
@@ -3243,44 +3255,44 @@
      iw = 0
 
      ! init sparse matrix C
-     ic(1) = 1
+     csrc%rowptr(1) = 1
 
      q = 0
-     do i=1,nrows
-         do ka=ia(i),ia(i+1)-1
+     do i=1,csra%nrows
+         do ka=csra%rowptr(i),csra%rowptr(i+1)-1
              q = q + 1
-             k = ja(ka)
+             k = csra%colptr(ka)
              iw(k) = q
-             jc(q) = k
-             c(q) = a(ka)
-         enddo ! over ka={ia(i),ia(i+1)-1} loop
+             csrc%colptr(q) = k
+             csrc%V(q) = csra%V(ka)
+         enddo ! over ka={csra%rowptr(i),csra%rowptr(i+1)-1} loop
          !
-         do kb=ib(i),ib(i+1)-1
-             k = jb(kb)
+         do kb=csrb%rowptr(i),csrb%rowptr(i+1)-1
+             k = csrb%colptr(kb)
 
              p = iw(k)
              if ( p == 0 ) then
                  q = q + 1
                  iw(k) = q
-                 jc(q) = k
-                 c(q) = b(kb)
+                 csrc%colptr(q) = k
+                 csrc%V(q) = csrb%V(kb)
              else
-                 c(p) = c(p) + b(kb)
+                 csrc%V(p) = csrc%V(p) + csrb%V(kb)
              endif ! back if ( p == 0 ) block
-         enddo ! over kb={ib(j),ib(j+1)-1} loop
+         enddo ! over kb={csrb%rowptr(j),csrb%rowptr(j+1)-1} loop
 
          ! done this row i, so set work array to zero again
-         do k=ic(i),q
-             iw( jc(k) ) = 0
-         enddo ! over k={ic(i),q} loop
-         ic(i+1) = q + 1
-     enddo ! over i={1,nrows} loop
+         do k=csrc%rowptr(i),q
+             iw( csrc%colptr(k) ) = 0
+         enddo ! over k={csrc%rowptr(i),q} loop
+         csrc%rowptr(i+1) = q + 1
+     enddo ! over i={1,csra%nrows} loop
 
      ! check the number of nonzero elements
-     if ( q > nnz ) then
+     if ( q > csrc%nnz ) then
          write(mystd,'(a)') 'sparse: error in csr_plus_d_t'
          STOP
-     endif ! back if ( q > nnz ) block
+     endif ! back if ( q > csrc%nnz ) block
 
 !! body]
 
@@ -3321,7 +3333,19 @@
 !! [body
 
      ! check dimensions
-     if ( nrows <= 0 .or. ncols <= 0 .or. nnz <= 0 ) then
+     if ( csra%nrows /= csrb%nrows .or. &
+          csra%nrows /= csrc%nrows .or. &
+          csrb%nrows /= csrc%nrows .or. &
+          csra%ncols /= csrb%ncols .or. &
+          csra%ncols /= csrc%ncols .or. &
+          csrb%ncols /= csrc%ncols ) then
+         write(mystd,'(a)') 'sparse: wrong dimensions for sparse matrix'
+         STOP
+     endif ! back if block
+     !
+     if ( csra%nnz /= csrb%nnz .or. &
+          csra%nnz /= csrc%nnz .or. &
+          csrb%nnz /= csrc%nnz ) then
          write(mystd,'(a)') 'sparse: wrong dimensions for sparse matrix'
          STOP
      endif ! back if block
@@ -3333,7 +3357,7 @@
      ic(1) = 1
 
      q = 0
-     do i=1,nrows
+     do i=1,csra%nrows
          do ka=ia(i),ia(i+1)-1
              q = q + 1
              k = ja(ka)
@@ -3361,13 +3385,13 @@
              iw( jc(k) ) = 0
          enddo ! over k={ic(i),q} loop
          ic(i+1) = q + 1
-     enddo ! over i={1,nrows} loop
+     enddo ! over i={1,csra%nrows} loop
 
      ! check the number of nonzero elements
-     if ( q > nnz ) then
+     if ( q > csrc%nnz ) then
          write(mystd,'(a)') 'sparse: error in csr_plus_z_t'
          STOP
-     endif ! back if ( q > nnz ) block
+     endif ! back if ( q > csrc%nnz ) block
 
 !! body]
 
